29450900edd19f15a9c4d7d147c0d757dd30e995
Ashish <Ashish@ASHISH-PC>
2026-08-08 18:21:20 +0530

    Update API routes, add Swagger, and configure MediatR

--- FlightManagement.API/Program.cs
+++ FlightManagement.API/Program.cs
@@ -1,20 +1,38 @@
+using FlightManagement.Application.Features.Airport.Queries;
+using FlightManagement.Domain.Interfaces;
+using FlightManagement.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(GetAllAirportsQuery).Assembly); });
+builder.Services.AddTransient<IAirportRepository, AirportRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAngular", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:4200")
+//        .AllowAnyMethod()
+//        .AllowAnyHeader()
+//        .AllowCredentials();
+//    });
+//});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
